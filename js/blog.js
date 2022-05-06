f6a29d4f8539d50734a5115c32cc3ed408bcb23b
hreinn <hreinngy@gmail.com>
2022-05-06 18:47:44 +0200

    Added "View more posts" functionality button as specified in the brief

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,8 @@
 const blogPostsContainer = document.querySelector(".blog-posts-container");
 const url = "https://hreinngylfason.site/projectexam/wp-json/wp/v2/posts?_embed&per_page=12";
+const viewMoreButton = document.querySelector(".view-more-btn button");
 
 getListPosts(blogPostsContainer, url, getBlogs);
-
-const viewMoreButton = document.querySelector(".view-more-btn button");
 
 viewMoreButton.onclick = () => {
   getListPosts(blogPostsContainer, url, getBlogs);
