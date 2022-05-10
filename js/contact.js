a4d810de58f5afd71ab95ece207d3e34b8f5acba
hreinn <hreinngy@gmail.com>
2022-05-10 16:55:24 +0200

    Contact page js validation done

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 const inputs = document.querySelectorAll("form > div :nth-child(2)");
 const nameError = "Name must be more than 5 characters";
-const emailError = "Email must be in valid format";
+const emailError = "Email must be in a valid format";
 const subjectError = "Subject must be more than 15 characters";
 const messageError = "Message must be more than 25 characters";
 
@@ -24,38 +24,60 @@ inputs.forEach((item) => {
     item.addEventListener("blur", function () {
       if (item.value) {
         if (item.id === "name") {
-          validateOnBlur(this, item.value, 5, nameError)
+          validateString(this, item.value, 5, nameError)
         }
         if (item.id === "email") {
-          if (!validateEmail(this, item.value)) {
-            document.querySelector(`#${this.id} ~ .input-error`).innerHTML = emailError;
-            this.classList.add("input-error-border")
-          } else {
-            this.classList.remove("input-error-border")
-          }
+          validateString(this, item.value, null, emailError)
         }
         if (item.id === "subject") {
-          validateOnBlur(this, item.value, 15, subjectError)
+          validateString(this, item.value, 15, subjectError)
         }
         if (item.id === "message") {
-          validateOnBlur(this, item.value, 25, messageError)
+          validateString(this, item.value, 25, messageError)
         }
       }
     })
   })
 })
 
-form.addEventListener("submit", function () {
-  
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = document.querySelector("#name")
+  const email = document.querySelector("#email")
+  const subject = document.querySelector("#subject")
+  const message = document.querySelector("#message")
+
+  const nameVal = validateString(name, name.value, 5, nameError)
+  const emailVal = validateString(email, email.value, null, emailError)
+  const subjectVal = validateString(subject, subject.value, 15, subjectError)
+  const messageVal = validateString(message, message.value, 25, messageError)
+
+  if (nameVal && emailVal && subjectVal && messageVal) {
+    document.querySelector(".form-success").style.display = "block";
+    form.reset();
+  }
 })
 
-function validateOnBlur(elem, value, length, errorMsg) {
-  if (!checkLength(elem, value, length)) {
-    elem.classList.add("input-error-border")
-    document.querySelector(`#${elem.id} ~ .input-error`).innerHTML = errorMsg;
+function validateString(elem, value, length, errorMsg) {
+  if (elem.id === "email") {
+    if (!validateEmail(elem, elem.value)) {
+      document.querySelector(`#${elem.id} ~ .input-error`).innerHTML = emailError;
+      elem.classList.add("input-error-border")
+      return false
+    } else {
+      elem.classList.remove("input-error-border")
+      return true
+    }
   } else {
-    elem.classList.remove("input-error-border")
-    document.querySelector(`#${elem.id} ~ .input-error`).innerHTML = "";
+    if (!checkLength(elem, value, length)) {
+      elem.classList.add("input-error-border")
+      document.querySelector(`#${elem.id} ~ .input-error`).innerHTML = errorMsg;
+      return false
+    } else {
+      elem.classList.remove("input-error-border")
+      document.querySelector(`#${elem.id} ~ .input-error`).innerHTML = "";
+      return true;
+    }
   }
 }
 
