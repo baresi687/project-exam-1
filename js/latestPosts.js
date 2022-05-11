404893371d343e9a82ee10b4f130ffdb6fa03eea
hreinn <hreinngy@gmail.com>
2022-05-11 15:48:58 +0200

    Added position dots on latest posts section on home page

--- js/latestPosts.js
+++ js/latestPosts.js
@@ -1,13 +1,37 @@
 const arrowLeft = document.querySelector(".fa-angle-left");
 const arrowRight = document.querySelector(".fa-angle-right");
 const scrollValue = document.querySelector(".posts")
+const dots = document.querySelectorAll(".dots");
+
+let slideIndex = 0;
+
+activeDot(slideIndex);
 
 arrowLeft.onclick = () => {
   scrollValue.scrollLeft -= 832;
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = 2;
+    scrollValue.scrollLeft = 1664;
+  }
+  activeDot(slideIndex);
 }
 
 arrowRight.onclick = () => {
   scrollValue.scrollLeft += 832;
+  slideIndex++;
+  if (slideIndex > 2) {
+    slideIndex = 0;
+    scrollValue.scrollLeft = 0;
+  }
+  activeDot(slideIndex);
+}
+
+function activeDot(index) {
+  dots.forEach((item) => {
+    item.classList.remove("active");
+  })
+  dots[index].classList.add("active");
 }
 
 const postsContainer = document.querySelector(".posts");
