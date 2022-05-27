56bbb8d25d80d9cd6af0943783b198095c00d387
hreinn <hreinngy@gmail.com>
2022-05-27 11:31:12 +0200

    Messenger in-built browser comment debugging test #19

--- js/components/postToWordPress.js
+++ js/components/postToWordPress.js
@@ -8,7 +8,6 @@ export async function postToWordPress(endPoint, data, elem, message1, message2) 
     })
     await response;
     response.ok ? displayMessage(elem, "success-message", message1, message2) : displayMessage(elem, "error-message");
-    return response;
 
   } catch (error) {
     displayMessage(elem, "error-message");
